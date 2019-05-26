bf7a850d323b798ea567a8848ed72210a1c3f8ab
Giordano O D Santos <giordanods@MacBook-Air-de-Giordano.local>
2019-05-25 23:53:48 -0300

    ajustes nas cores dos usuários

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
           <Jogador corFundo="gray"/>
         </View>
         <View style={styles.linha}>
-          <Jogador />
+          <Jogador corFundo="red" />
         </View>
       </View>
     );
